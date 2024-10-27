ba57c9bbf2e0a32131ecd606cadc79bf6c079363
GarfieldTom <3176246516@qq.com>
2024-10-27 17:54:54 +0800

    CREATION
    初版本程序，由烟花易冷（https://www.zzfly.net/tybetter/）开发。之后的开发都基于这个初代的版本。
    Initial version of the programme, developed by Smokey (https://www.zzfly.net/tybetter/). Subsequent developments are based on this initial version.

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * 本主题由Rytia（By烟花易冷 zzfly.net）基于Typecho官方主题二次修改制作，全盘进行Material Design化，保持官方主题的精简与快捷，具有访问迅速、SEO良好的特点，适用于小内存VPS或者是网络不算快的网站使用，演示站：http://www.getlinux.xyz
+ * 
+ * @package TyBetter 
+ * @author Typecho & Rytia
+ * @version 1.21
+ * @link http://www.zzfly.net
+ */
+
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+ $this->need('header.php');
+ ?>
+
+<div class="col-mb-12 col-8" id="main" role="main">
+	<?php while($this->next()): ?>
+        <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
+			<h2 class="post-title" itemprop="name headline"><a itemtype="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
+			<ul class="post-meta">
+				<li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
+				<li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('F j, Y'); ?></time></li>
+				<li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
+				<li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
+			</ul>
+            <div class="post-content" itemprop="articleBody">
+
+    			<?php $this->excerpt(100, '');//200就是摘要的字数 ?>            </div>
+        </article>
+	<?php endwhile; ?>
+
+    <?php $this->pageNav('&laquo; 前一页', '后一页 &raquo;'); ?>
+</div><!-- end #main-->
+
+<?php $this->need('sidebar.php'); ?>
+<?php $this->need('footer.php'); ?>
---
+++ readme.md
@@ -0,0 +1,5 @@
+![封面](https://github.com/little-gt/THEME-TyBetter/screenshot.png)
+
+> 基于Typecho官方主题二次修改制作，全盘进行Material Design化，保持官方主题的精简与快捷，具有访问迅速、SEO良好的特点，适用于小内存VPS或者是网络不算快的网站使用。
+> 作者：[烟花易冷](https://www.zzfly.net/tybetter/)
+> 二次开发：@little-gt
